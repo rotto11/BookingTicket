fd2a48dab5915eb159671586f10ab827fb417d86
daffa17ti <41132206+daffa17ti@users.noreply.github.com>
2018-08-02 04:41:31 +0700

    Delete koneksi.php

--- koneksi.php
+++
@@ -1,8 +0,0 @@
-<?php
-$con=mysqli_connect("localhost","root","123456","prot_bpwl");
-if(mysqli_connect_errno($con))
-{echo "Koneksi dengan mysql gagal" .mysqli_connect_error;
-}
-else
-{echo "";}
-?>
